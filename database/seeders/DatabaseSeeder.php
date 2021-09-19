5865a9787080b9332e45cbd4f2718d47b39a68ae
momtaz <momtaz_nussair@yahoo.com>
2021-09-19 16:50:07 +0200

    updated header view , solved seed problem

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         $this->call([
-            CreateAdminUSerSeeder::class,
             PermissionTableSeeder::class,
+            CreateAdminUSerSeeder::class,
             StatusSeeder::class,
         ]);
     }
