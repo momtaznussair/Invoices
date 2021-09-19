0ffef083b4dea8ef8a5d9574a0e88ed7a80d7ef5
momtaz <momtaz_nussair@yahoo.com>
2021-09-19 15:39:24 +0200

    heroku config file

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Footer opened -->
 	<div class="main-footer ht-40">
 		<div class="container-fluid pd-t-0-f ht-100p">
-			<span>Copyright © 2021 <a href="mailto:momtaznussair97@gmail.com?subject=Invoices management system">Momtaz Nussair</a></span>
+			<span>Copyright © 2021 <a href="mailto:momtaznussair97@gmail.com?subject=Invoices management system" target="_blank">Momtaz Nussair</a></span>
 		</div>
 	</div>
 <!-- Footer closed -->
